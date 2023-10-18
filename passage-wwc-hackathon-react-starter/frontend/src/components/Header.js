467f1e167f55dbf75b7caccc9cfe277b40cf9450
Damanpreet Kaur <bhullars@Damanpreets-Mac-mini.local>
2023-10-18 14:18:58 -0400

    changes naming for elements

---
+++ passage-wwc-hackathon-react-starter/frontend/src/components/Header.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import styles from "../styles/Header.module.css";
+import LogoutButton from "./LogoutButton";
+
+function Header() {
+  return (
+    <div className={styles.mainHeader}>
+      <Link to={"/"}>
+        <div className={styles.passageLogo}></div>
+      </Link>
+      <div className={styles.headerText}>Hackathon Project</div>
+      <div className={styles.spacer}></div>
+      <Link to={"/profile"}>Profile</Link>
+      <Link to={"/login"}>Login</Link>
+      <Link to={"/register"}>Register</Link>
+      <LogoutButton />
+    </div>
+  );
+}
+export default Header;
