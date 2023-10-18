22aa665b5ff4b80616f5a1cc025e63dbd69bdc1c
emcclellan <85776223+emcclellan@users.noreply.github.com>
2023-10-17 19:31:07 -0700

    removed Register.js and references.  Changed the PassageLogin to use PassageAuth which handles both register and login

--- passage-wwc-hackathon-react-starter/frontend/src/app.js
+++ passage-wwc-hackathon-react-starter/frontend/src/app.js
@@ -7,7 +7,6 @@ import Dashboard from "./views/Dashboard";
 import Profile from "./views/Profile";
 import Banner from "./components/banner";
 import styles from "./styles/App.module.css";
-import {Register} from "./components/Register";
 import {Login} from "./components/Login";
 
 function App() {
@@ -20,7 +19,6 @@ function App() {
             <Route path="/" element={<Home />}></Route>
             <Route path="/dashboard" element={<Dashboard />}></Route>
             <Route path="/profile" element={<Profile />}></Route>
-            <Route path="/register" element={<Register />}></Route>
             <Route path="/login" element={<Login/>}></Route>
           </Routes>
         </div>
