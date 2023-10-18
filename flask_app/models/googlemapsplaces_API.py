144a1e483e0756173b8e2ef64d4aaeb4ceaceaf9
Sarah Sotomayor <sarah@i0119Mac.local>
2023-10-18 14:09:41 -0600

    decreased search radius to 15 miles, 24140 meters

--- flask_app/models/googlemapsplaces_API.py
+++ flask_app/models/googlemapsplaces_API.py
@@ -15,7 +15,7 @@ gmaps = googlemaps.Client(key = API_KEY)
 # define Search
     # TODO: ADD GEOLOCATION API FOR LAT & LONG COORDINATES (current coordinates are for testing)
     # TODO: REVIEW & CUSTOMIZE RADIUS
-places_result = gmaps.places_nearby(location= '39.729432, -104.831920', radius = 40000, type = 'animal shelter')
+places_result = gmaps.places_nearby(location= '39.729432, -104.831920', radius = 24140, type = 'animal shelter')
 
 # test print
 pprint.pprint(places_result)
@@ -25,6 +25,7 @@ for place in places_result['results']:
     
     my_place_id = place['place_id']
     
+    #testing some fields
     my_fields = ['name', 'formatted_phone_number', 'type']
     
     place_details = gmaps.place(place_id = my_place_id, fields = my_fields)
