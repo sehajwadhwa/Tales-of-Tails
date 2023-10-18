20ea8dc2bec9a03517d2d730d6724d0aa2f51d85
Sarah Sotomayor <sarah@i0119Mac.local>
2023-10-18 12:49:01 -0600

    added imports

---
+++ flask_app/models/googlemapsplaces_API.py
@@ -0,0 +1,18 @@
+import googlemaps
+import pprint
+import time
+from GoogleMapsAPIKeY import get_my_key
+
+
+# define API key
+API_KEY = get_my_key()
+
+# define Client to authenticate ourselves
+gmaps = googlemaps.Client(key = API_KEY)
+
+# define Search
+
+
+
+
+
