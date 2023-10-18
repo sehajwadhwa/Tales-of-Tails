47f6b3712bc667314bc07e6e8c65ca955a84c199
Sarah Sotomayor <sarah@i0119Mac.local>
2023-10-18 13:13:02 -0600

    adding API set up

--- flask_app/models/googlemapsplaces_API.py
+++ flask_app/models/googlemapsplaces_API.py
@@ -1,18 +1,21 @@
 import googlemaps
 import pprint
 import time
-from GoogleMapsAPIKeY import get_my_key
+import os
 
-
-# define API key
-API_KEY = get_my_key()
+# define API_KEY
+API_KEY = os.environ.get("GMAPS_API_KEY")
 
 # define Client to authenticate ourselves
 gmaps = googlemaps.Client(key = API_KEY)
 
 # define Search
+    # TODO: ADD GEOLOCATION API FOR LAT & LONG COORDINATES 
+    # TODO: REVIEW & CUSTOMIZE RADIUS
+places_result = gmaps.places_nearby(location= '', radius = 40000, type = 'animal shelter')
 
-
+# test print
+pprint.pprint(places_result)
 
 
 
