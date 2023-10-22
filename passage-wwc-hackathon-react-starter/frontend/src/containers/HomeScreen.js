3bfe92f28d772b89ee06e768e8a4083857d42c2a
emcclellan <85776223+emcclellan@users.noreply.github.com>
2023-10-22 15:24:21 -0700

    remove controllertest

--- passage-wwc-hackathon-react-starter/frontend/src/containers/HomeScreen.js
+++ passage-wwc-hackathon-react-starter/frontend/src/containers/HomeScreen.js
@@ -41,7 +41,6 @@ const HomeScreen = () => {
   return (
     
     <div className="home-screen-wrapper">
-      <ControllerTest/>
       <div className="banner-wrapper">
         <div className="l-container">
           <img className="yellow-cat" src={yellowCat} />
